66f428ce58f6a2125177549d017ef6d89df1b7a9
bxd718 <edsonfenias718@gmai.com>
2024-07-01 14:24:50 +0200

    Add: adding random response messages to simulate human behavior

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ const client = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
     args: ["--no-sandbox"],
-    headless: true,
+    headless: false,
   },
   webVersionCache: {
     type: "remote",
@@ -46,12 +46,55 @@ client.on("ready", () => {
   logger.info("READY");
 });
 
+
+const respostasRecarregamento = [
+  "Conta recarregada com sucesso!",
+  "Recarga realizada com êxito!",
+  "Feito! Conta recarregada.",
+  "Tudo certo! Recarregamento concluído.",
+  "Pronto! Conta agora está recarregada.",
+  "Concluído! Recarga efetuada.",
+  "Sucesso! Conta foi recarregada.",
+  "Feito! Recarga processada.",
+  "Obrigado! Conta recarregada.",
+  "Pronto para uso! Recarga completa.",
+  "Operação realizada com sucesso!",
+  "Feito com sucesso! Conta está recarregada.",
+  "Excelente! Recarga concluída.",
+  "Concluído! Recarregamento realizado.",
+  "Muito bem, conta recarregada."
+];
+
+const errorFormat= [
+  'Por favor, certifique-se de copiar a mensagem de confirmação exatamente como fornecido e adicionar o número de celular associado à conta Yango no fim do corpo da mensagem para o Recarregamento!',
+  'Certifique-se de incluir corretamente a mensagem de confirmação e o número de celular associado à conta Yango para completar o Recarregamento.',
+  'Para completar o Recarregamento, é necessário copiar a mensagem de confirmação exatamente como fornecido e adicionar o número de celular associado à conta Yango no fim do corpo da mensagem.',
+  'Por favor, adicione o número de celular associado à conta Yango no fim do corpo da mensagem de confirmação para finalizar o Recarregamento.'
+];
+
+const errorGeral = [
+  'Ocorreu um erro durante o recarregamento, provavelmente o número que colocou não está associado a nenhuma conta Yango neste parceiro. Por favor, tente novamente! Se o problema persistir, entre em contato com o suporte!',
+  'Infelizmente, houve um problema com o recarregamento. Verifique se o número fornecido está associado à sua conta Yango. Se precisar de ajuda, entre em contato com o suporte.',
+  'Desculpe, não conseguimos completar o recarregamento. Certifique-se de que o número inserido corresponde à sua conta Yango. Entre em contato com o suporte se precisar de assistência adicional.',
+  'Houve um erro ao tentar recarregar sua conta. Verifique o número associado à sua conta Yango e tente novamente. Caso persista o problema, entre em contato com o suporte.',
+  'Ops! Parece que ocorreu um problema durante o recarregamento. Verifique os detalhes fornecidos e tente novamente.',
+  'Desculpe, não conseguimos processar seu pedido de recarregamento. Verifique o número de celular e tente novamente.',
+  'Erro ao recarregar conta. Certifique-se de seguir as instruções corretamente e tente novamente.',
+  'Não foi possível completar o recarregamento. Por favor, verifique as informações fornecidas e tente novamente mais tarde.',
+  'Desculpe, ocorreu um problema técnico durante o recarregamento. Tente novamente mais tarde ou entre em contato com o suporte.'
+];
+
+
 client.on("message", async msg => {
   let message = msg.body;
-  let senderID = msg.from;
+  // let senderID = msg.from;
+
   logger.info("MESSAGE RECEIVED: " + JSON.stringify(msg));
+
    try {
     const processor = getProcessor(message);
+
+    // Chamar suporte caso a mensagem enviada não seja um comprovativo para recarregamento
    if (!processor) {
   // Inicializar o estado do usuário, se não estiver definido
   // if (!userStates[senderID]) {
@@ -68,7 +111,7 @@ client.on("message", async msg => {
   // }
 
   return;
-}
+  }
 
     const transaction = processor.process(message);
 
@@ -79,31 +122,57 @@ client.on("message", async msg => {
 
     if (!result.success || transaction.contact === 'N/A') {
       // const errorMessage =`Por favor, certifique-se de copiar a mensagem de confirmação exatamente como fornecido e adicionar o contato para o recarregamento conforme o exemplo abaixo:\n\n${processor.getExampleMessage()}\n\nPor favor, reenvie a mensagem corretamente seguindo o formato acima para completar o processo de recarga. Caso o problema persista, a recarga pode ter sido usada ou adulterada! Em caso de dúvidas, entre em contacto com o suporte!`.trimStart();
-      const errorMessage = 'Por favor, certifique-se de copiar a mensagem de confirmação exactamente como fornecido e adicionar o numero de cellular associado a conta Yango no fim do corpo da mensagem para o Recarregamento!';
+      const errorMessage = selecionarMensagemDeErroAleatoria();
       logger.info(errorMessage);
-      msg.reply(errorMessage);
+      //Chamar o metodo para enviar a mensagem com algum Erro
+      enviarMensagemComAtraso(msg, errorMessage);
       return;
     }
 
     const createResult = await createFleetTransaction(transaction.contact, transaction.amount, transaction.id);
     if (!createResult.success) {
-      logger.info(
-        "Ocorreu um erro durante o recarregamento. Por favor, tente novamente! Se o problema persistir, entre em contacto com o suporte!",
-      );
-      msg.reply(
-        "Ocorreu um erro durante o recarregamento. Por favor, tente novamente! Se o problema persistir, entre em contacto com o suporte!",
-      );
+      const answerGeralError = selecionarMensagemGeralDeErroAleatoria();
+      logger.info(answerGeralError);
+        //Chamar o metodo para enviar a mensagem com algum atraso
+      enviarMensagemComAtraso(msg, answerGeralError);
+      
       return;
     }
 
     await markTransactionAsCompleted(transaction);
-    logger.info(`Conta Recarregada: ${JSON.stringify(message)}`);
-    msg.reply("Conta recarregada!");
+    const answerSucess = selecionarRespostaAleatoria();
+    logger.info('Conta recarregada!');
+    //Chamar o metodo para enviar a mensagem com algum atraso
+    enviarMensagemComAtraso(msg, answerSucess);
   } catch (error) {
     logger.error(`Ocorreu um erro: ${error}`);
     //msg.reply("Desculpe, ocorreu um erro inesperado. Por favor, tente novamente mais tarde.");
   }
 });
+
+// Função para selecionar um resposta de erro aleatoria
+function selecionarRespostaAleatoria() {
+  const indiceAleatorio = Math.floor(Math.random() * respostasRecarregamento.length);
+  return respostasRecarregamento[indiceAleatorio];
+}
+
+// Função para selecionar uma mensagem de erro aleatória
+function selecionarMensagemDeErroAleatoria() {
+  const indiceAleatorio = Math.floor(Math.random() * errorFormat.length);
+  return errorFormat[indiceAleatorio];
+}
+
+function enviarMensagemComAtraso(msg: any, mensagem: string) {
+  const tempoAtraso = Math.random() * 5000 + 2000; // Atraso aleatório entre 2000ms e 7000ms
+  setTimeout(() => {
+      msg.reply(mensagem);
+  }, tempoAtraso);
+}
+
+function selecionarMensagemGeralDeErroAleatoria() {
+  const indiceAleatorio: number = Math.floor(Math.random() * errorGeral.length);
+  return errorGeral[indiceAleatorio];
+}
 
 function getProcessor(message: string): MessageProcessor | null {
   if (message.substring(0, 2) === "ID") {
